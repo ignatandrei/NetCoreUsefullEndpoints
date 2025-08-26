23e928c9108ef2b469bb36c8b95eb624b165e7d7
Andrei Ignat <ignatandrei@yahoo.com>
2025-08-26 05:16:31 +0300

    add docs and version

--- src/UsefullEndpoints/UsefullEndpoints.AppHost/Program.cs
+++ src/UsefullEndpoints/UsefullEndpoints.AppHost/Program.cs
@@ -1,5 +1,6 @@
+using WebAPIDocsExtensionAspire;
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.TestUsefullEndpoints>("testusefullendpoints");
-
+var webAPI = builder.AddProject<Projects.TestUsefullEndpoints>("testusefullendpoints");
+webAPI.AddSDKGeneration_openapitools(jsonPath: "swagger/v1/swagger.json");
 builder.Build().Run();
